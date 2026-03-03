81ed95166c0ab123eddc15c33cadb6635b77a215
mattnickolaus <mattnickolaus@gmail.com>
2026-03-02 22:26:40 -0500

    adding forward and backward text search

--- mim.c
+++ mim.c
@@ -466,15 +466,38 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
+  if (last_match == -1)
+    direction = 1;
+  int current = last_match;
+
   for (int i = 0; i < E.numrows; i++) {
-    erow_t *row = &E.row[i];
+    current += direction;
+    if (current == -1)
+      current = E.numrows - 1;
+    else if (current == E.numrows)
+      current = 0;
+
+    erow_t *row = &E.row[current];
     char *match = strstr(row->render, query);
     if (match) {
-      E.cy = i;
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
@@ -488,7 +511,9 @@ void editorFind() {
   int saved_coloff = E.coloff;
   int saved_rowoff = E.rowoff;
 
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char *query =
+      editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+
   if (query) {
     free(query);
   } else {
